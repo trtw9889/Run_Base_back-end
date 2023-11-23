af17763408f6c632bfdb38b95621abb51d86938a
ella <trtw9889@gmail.com>
2023-11-23 15:15:39 +0900

    MODIFY : 상품리스트 필터,정렬 수정완료

--- runbase/src/products/products.controller.ts
+++ runbase/src/products/products.controller.ts
@@ -23,33 +23,8 @@ export class ProductsController {
     return this.productsService.getNewProduct(page, perPage);
   }
 
-  @Get('clothes')
-  async getProductsByClothes(
-    @Query('page') page: number,
-    @Query('perPage') perPage: number,
-  ): Promise<Products[]> {
-    return this.productsService.getProductsByClothes(page, perPage);
-  }
-
   @Get('shoes')
   async getProductsByShoes(
-    @Query('page') page: number,
-    @Query('perPage') perPage: number,
-  ): Promise<Products[]> {
-    return this.productsService.getProductsByShoes(page, perPage);
-  }
-
-  @Get('goods')
-  async getProductsByGoods(
-    @Query('page') page: number,
-    @Query('perPage') perPage: number,
-  ): Promise<Products[]> {
-    return this.productsService.getProductsByGoods(page, perPage);
-  }
-
-  @Get('sort/:categoryId')
-  async sortAndFilterProducts(
-    @Param('categoryId') categoryId: number,
     @Query('page') page: number,
     @Query('perPage') perPage: number,
     @Query('sorting') sorting: string,
@@ -62,8 +37,59 @@ export class ProductsController {
     const colors = color ? color.split(',').map((id) => parseInt(id)) : [];
     const sizes = size ? size.split(',').map((id) => parseInt(id)) : [];
 
-    return this.productsService.sortAndFilterProducts(
-      categoryId,
+    return this.productsService.getProductsByShoes(
+      page,
+      perPage,
+      sorting,
+      colors,
+      minPrice ? minPrice : null,
+      maxPrice ? maxPrice : null,
+      sizes,
+      gender,
+    );
+  }
+
+  @Get('clothes')
+  async getProductsByClothes(
+    @Query('page') page: number,
+    @Query('perPage') perPage: number,
+    @Query('sorting') sorting: string,
+    @Query('color') color: string,
+    @Query('minPrice') minPrice: string,
+    @Query('maxPrice') maxPrice: string,
+    @Query('size') size: string,
+    @Query('gender') gender: number,
+  ): Promise<Products[]> {
+    const colors = color ? color.split(',').map((id) => parseInt(id)) : [];
+    const sizes = size ? size.split(',').map((id) => parseInt(id)) : [];
+
+    return this.productsService.getProductsByClothes(
+      page,
+      perPage,
+      sorting,
+      colors,
+      minPrice ? minPrice : null,
+      maxPrice ? maxPrice : null,
+      sizes,
+      gender,
+    );
+  }
+
+  @Get('goods')
+  async getProductsByGoods(
+    @Query('page') page: number,
+    @Query('perPage') perPage: number,
+    @Query('sorting') sorting: string,
+    @Query('color') color: string,
+    @Query('minPrice') minPrice: string,
+    @Query('maxPrice') maxPrice: string,
+    @Query('size') size: string,
+    @Query('gender') gender: number,
+  ): Promise<Products[]> {
+    const colors = color ? color.split(',').map((id) => parseInt(id)) : [];
+    const sizes = size ? size.split(',').map((id) => parseInt(id)) : [];
+
+    return this.productsService.getProductsByGoods(
       page,
       perPage,
       sorting,
